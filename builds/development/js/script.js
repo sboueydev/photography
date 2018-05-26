dc91b6f22ba2c06c232ecbf035c4cdf53556a079
sboueydev <sbouey.dev@gmail.com>
2018-05-25 21:18:37 -0700

    Test and optimize display for several viewport dimensions

--- builds/development/js/script.js
+++ builds/development/js/script.js
@@ -1,4 +1,27 @@
-$('.horizontal-wrapper').mousewheel(function(e, delta) {
-    this.scrollLeft -= (delta * 40);
-    e.preventDefault();
+$( document ).ready(function() {
+    $('.horizontal-wrapper').mousewheel(function(e, delta) {
+        this.scrollLeft -= (delta * 40);
+        e.preventDefault();
+    });
+    var vw = window.innerWidth;
+    var vh = window.innerHeight;
+    if(
+        (vw === 1366 && vh === 768)
+    ) {
+        $('.hide-scroll').css('height', '405px');
+    } else if(vw === 1024 & vh === 768) {
+        $('.hide-scroll').css('height', '415px');
+    } else if(vw === 1280 && vh === 800) {
+        $('.hide-scroll').css('height', '445px');
+    } else if(
+        (vw === 1366 && vh === 1024) ||
+        (vw === 1920 && vh === 1200)) {
+        $('.hide-scroll').css('height', '620px');
+    } else if (
+        (vw === 768 && vh === 1024) ||
+        (vw === 1024 & vh === 1366)) 
+    {
+        $('#home-gallery').removeClass('horizontal-wrapper');
+        $('#home-gallery').addClass('column-view');
+    }
 });
